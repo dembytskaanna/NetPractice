2a041e0f35a078a550869f032c383079c27896ce
YevheniiPopov <eugenep.exe@gmail.com>
2024-04-27 18:09:40 +0300

    New project 1st commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
